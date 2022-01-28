4c01d0b3d4152a9ec15502652326b0ae204db0ab
CSR2017 <suganya@imanisoftware.com>
2022-01-27 18:11:55 -0600

    Fix hard coded paths

--- src/EdFi.SampleDataGenerator.Console/Entities/Csv/CsvHelper.cs
+++ src/EdFi.SampleDataGenerator.Console/Entities/Csv/CsvHelper.cs
@@ -8,26 +8,27 @@ namespace EdFi.SampleDataGenerator.Console.Entities.Csv
 {
     public static class CsvHelper
     {
-        // EducationOrganization
-        public static string BasePath = @".\Samples\SampleDataGenerator\DataFiles\";
-        public const string AccountabilityRatingPath = @"EducationOrganization\AccountabilityRating.csv";
-        public const string ClassPeriodPath = @"EducationOrganization\ClassPeriod.csv";
-        public const string CoursePath = @"EducationOrganization\Course.csv";
-        public const string EducationServiceCenterPath = @"EducationOrganization\EducationServiceCenter.csv";
-        public const string LocalEducationAgencyPath = @"EducationOrganization\LocalEducationAgency.csv";
-        public const string LocationPath = @"EducationOrganization\Location.csv";
-        public const string ProgramPath = @"EducationOrganization\Program.csv";
-        public const string SchoolPath = @"EducationOrganization\School.csv";
+       
+        private static string basePath = $"{Path.Combine(".","Samples","SampleDataGenerator","DataFiles")}{Path.DirectorySeparatorChar}";    
 
-        // EducationOrgCalendar
-        public const string CalendarPath = @"EducationOrgCalendar\Calendar.csv";
-        public const string CalendarDatePath = @"EducationOrgCalendar\CalendarDate.csv";
-        public const string GradingPeriodPath = @"EducationOrgCalendar\GradingPeriod.csv";
-        public const string SessionPath = @"EducationOrgCalendar\Session.csv";
+        public static string BasePath { get => basePath; set => basePath = value; }
+        public static string AccountabilityRatingPath { get => ResourceFilePath("EducationOrganization", "AccountabilityRating.csv"); }
+        public static string ClassPeriodPath { get => ResourceFilePath("EducationOrganization", "ClassPeriod.csv"); }
+        public static string CoursePath { get => ResourceFilePath("EducationOrganization", "Course.csv"); }
+        public static string EducationServiceCenterPath { get => ResourceFilePath("EducationOrganization", "EducationServiceCenter.csv"); }
+        public static string LocalEducationAgencyPath { get => ResourceFilePath("EducationOrganization", "LocalEducationAgency.csv"); }
+        public static string LocationPath { get => ResourceFilePath("EducationOrganization", "Location.csv"); }
+        public static string ProgramPath { get => ResourceFilePath("EducationOrganization", "Program.csv"); }
+        public static string SchoolPath { get => ResourceFilePath("EducationOrganization", "School.csv"); }
 
-        // MasterSchedule
-        public const string CourseOfferingPath = @"MasterSchedule\CourseOffering.csv";
-        public const string SectionPath = @"MasterSchedule\Section.csv";
+        public static string CalendarPath { get => ResourceFilePath("EducationOrgCalendar", "Calendar.csv"); }
+        public static string CalendarDatePath { get => ResourceFilePath("EducationOrgCalendar", "CalendarDate.csv"); }
+        public static string GradingPeriodPath { get => ResourceFilePath("EducationOrgCalendar", "GradingPeriod.csv"); }
+        public static string SessionPath { get => ResourceFilePath("EducationOrgCalendar", "Session.csv"); }
+
+        public static string CourseOfferingPath { get => ResourceFilePath("MasterSchedule", "CourseOffering.csv"); }
+        public static string SectionPath { get => ResourceFilePath("MasterSchedule", "Section.csv"); }
+
 
         public static List<T> MapCsvToEntity<T,TU>(string path) where TU : CsvClassMap<T>
         {
@@ -47,6 +48,11 @@ namespace EdFi.SampleDataGenerator.Console.Entities.Csv
                 csv.Configuration.RegisterClassMap<TU>();
                 csv.WriteRecords(records);
             }
+        }
+
+        private static string ResourceFilePath(string folder, string filename)
+        {
+            return Path.Combine(folder, filename);
         }
     }
 }
